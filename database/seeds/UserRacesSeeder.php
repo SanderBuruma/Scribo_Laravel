4c4418a88acde8f31733c3705a0ceb74f2eb79de
Sander Buruma <sanderburuma@gmail.com>
2018-12-30 00:19:39 +0100

    .

--- database/seeds/UserRacesSeeder.php
+++ database/seeds/UserRacesSeeder.php
@@ -44,26 +44,5 @@ class UserRacesSeeder extends Seeder
         }
 
         StatsController::calculateAndSaveStats();
-        // $userStats = DB::select('SELECT user_id, name, sum(texts.length) as races_len, count(*) as races, sum(races.mistakes) as mistakes, sum(texts.length)/sum(races.time_taken)*12 AS WPM, sum(races.time_taken) as time_taken
-		// FROM races
-		// INNER JOIN texts ON races.text_id=texts.id
-		// INNER JOIN users ON races.user_id=users.id
-		// GROUP BY user_id
-		// ORDER BY WPM DESC'
-        // );
-        // $rank = 1;
-        // foreach ($userStats as $stat) {
-        //     $date1 = date('Y-m-d G:i:s', time()-random_int(0,1e6));
-        //     $user = User::find($stat->user_id);
-        //     $user->mistakes     = $stat->mistakes;
-        //     $user->rank         = $rank++;
-        //     $user->races        = $stat->races;
-        //     $user->time_taken   = $stat->time_taken;
-        //     $user->races_len    = $stat->races_len;
-        //     $user->stats_updated = time()-random_int(0,1e7);
-        //     $user->created_at = $date1;
-        //     $user->updated_at = $date1;
-        //     $user->save();
-        // }
     }
 }
